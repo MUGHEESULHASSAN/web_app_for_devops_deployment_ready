791defdb6af02dfadc5ff5cf02764008b69f6476
root <mugheesulhassan217@gmail.com>
2025-10-07 22:01:18 +0000

    first commit

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -5,6 +5,6 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/admin">
     <App /></BrowserRouter>
 )
